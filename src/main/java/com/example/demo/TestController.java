c275210300abcc28293a70682413b76b5903b5f0
zeze.li <lizeze.boy@gmail.com>
2020-07-30 13:38:52 +0800

    first commit

---
+++ src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ：lzz
+ * @BelongsProject: com.example.demo
+ * @date ：Created in 2020/7/30 13:36
+ * @description ：
+ * @modified By：
+ */
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+
+    @GetMapping("/")
+    public String Test() {
+
+        return "Docker";
+    }
+
+}
